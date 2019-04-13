3cd7046070320b628f079c0092e90fda64b2a8a7
supgb <supgb96@gmail.com>
2019-04-14 01:53:15 +0800

    irony

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 (require 'package)
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.emacs-china.org/gnu/"))
 
 (package-initialize)
 
@@ -36,12 +40,45 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-clang-executable "/usr/bin/clang")
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (flycheck-irony company-irony irony flycheck company-lsp lsp-ui cquery zygospore helm-gtags helm yasnippet ws-butler volatile-highlights-mode use-package undo-tree iedit dtrt-indent counsel-projectile clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(use-package lsp-mode
+  :commands lsp
+  :init
+  (setq ...))
+(setq lsp-clients-clangd-executable "/usr/bin/clang")
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
+(add-hook 'c++-mode-hook #'lsp)
+
+(setq cquery-executable "/home/sgb/cquery/release/bin/cquery")
+;; ;; Arch Linux aur/cquery-git aur/cquery
+;; (setq cquery-executable "/usr/bin/cquery")
+
+;; ;; Log file
+;; (setq cquery-extra-args '("--log-file=/tmp/cq.log"))
+;; ;; Cache directory, both relative and absolute paths are supported
+;; (setq cquery-cache-dir ".cquery_cached_index")
+;; ;; Initialization options
+;; (setq cquery-extra-init-params '(:cacheFormat "msgpack"))
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'irony-mode-hook #'irony-eldoc)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
